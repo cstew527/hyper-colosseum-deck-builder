99126e5e16364d776f95bd3f20e2cbe9ff89574f
feucroute <christopherstewart05@gmail.com>
2022-11-21 18:56:51 -0500

    added show page

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
 const express = require('express')
-const { default: mongoose } = require('mongoose')
+const mongoose = require('mongoose')
 const app = express()
 const Card = require('./models/cards.js')
 const cardInfo = require('./models/cardSeed.js')
 // const Deck = require('./models/cards.js')
 
+// Home page
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
+// Main card gallery page
 app.get('/cards', (req, res) => {
     Card.find({}, (err, allCards) => {
         res.render('card.ejs', {
-            card: allCards
+            cards: allCards
         })
     }).sort({
         cardName: 1
+    })
+})
+
+// Main card detail page
+app.get('/cards/:cardNumber', (req, res) => {
+    Card.findById(req.params.cardNumber, (err, foundCard) => {
+        res.render('show.ejs', {
+            card: foundCard
+        })
     })
 })
 
